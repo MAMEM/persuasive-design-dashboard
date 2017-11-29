38aff8f275e56f383ac22eda9349002b1d00b715
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-11-29 14:08:57 +0200

    Moved some functions around

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,122 @@
+"use strict";
 
+function createFBItems(arr) {
 
+    var posts = [];
 
+    for (var i=0; i < arr.length; i++) {
+
+        // Remove new line chars from title
+        var post_title = !!arr[i].message ? arr[i].message.replace(/\n/g, " ") : "Shared post: "+ arr[i].link;
+
+        var shared_post = arr[i].status_type === "shared_story";
+        var has_image = arr[i].type === "photo";
+        var has_video = arr[i].type === "video";
+
+        var media_post = (has_image | has_video);
+
+        var pub_time = String(Date.parse(arr[i].created_time));
+        pub_time = parseInt(pub_time.slice(0, -3), 10);
+
+        posts.push({
+            title: post_title,
+            id: arr[i].id,
+            pageUrl: arr[i].permalink_url,
+            publicationTime: pub_time,
+            original: !shared_post,
+            likes: arr[i].likes ? arr[i].likes : 0,
+            shares: arr[i].shares ? arr[i].shares : 0,
+            comments: arr[i].comments ? arr[i].comments : 0
+        });
+
+        posts[posts.length-1].type = media_post ? 'mediaItem' : 'item' ;
+
+        if (media_post) {
+            posts[posts.length-1].mediaType = has_image ? 'image' : 'video';
+            posts[posts.length-1].mediaUrl = has_image ?  arr[i].picture :  arr[i].source;
+            posts[posts.length-1].thumbnail = arr[i].picture;
+        }
+    }
+
+    return posts;
+}
+
+function createTwitterItems(arr) {
+
+    var items = [], retweet;
+
+    for (var i=0; i < arr.length; i++) {
+
+        // Remove new line chars from title
+        retweet = !arr[i].original;
+
+        var pub_time = String(arr[i].publicationTime);
+        pub_time = parseInt(pub_time.slice(0, -3), 10);
+
+        items.push({
+            title: arr[i].cleanTitle,
+            id: arr[i].id,
+            pageUrl: arr[i].pageUrl,
+            publicationTime: pub_time,
+            original: !retweet,
+            rt: retweet,
+            mentions: arr[i].mentions,
+            tags: arr[i].tags,
+            likes: arr[i].likes ? arr[i].likes : 0,
+            shares: arr[i].shares ? arr[i].shares : 0,
+            comments: arr[i].comments ? arr[i].comments : 0,
+            type: arr[i].type
+        });
+
+        if (items[items.length-1].type === 'mediaItem') {
+            items[items.length-1].mediaType = arr[i].mediaType;
+            items[items.length-1].mediaUrl = arr[i].mediaUrl;
+            items[items.length-1].thumbnail = arr[i].thumbnail;
+        }
+
+        if (retweet) {
+            items[items.length-1].reference = arr[i].reference;
+            items[items.length-1].referencedUserId = arr[i].referencedUserId;
+        }
+    }
+
+    return items;
+}
+
+function createGooglePlusItems(arr) {
+
+    var items = [], original;
+
+    for (var i=0; i < arr.length; i++) {
+
+        original = arr[i].original;
+
+        var pub_time = String(arr[i].publicationTime);
+        pub_time = parseInt(pub_time.slice(0, -3), 10);
+
+        items.push({
+            title: arr[i].cleanTitle,
+            id: arr[i].id,
+            pageUrl: arr[i].pageUrl,
+            publicationTime: pub_time,
+            original: original,
+            tags: arr[i].tags,
+            likes: arr[i].likes ? arr[i].likes : 0,
+            shares: arr[i].shares ? arr[i].shares : 0,
+            comments: arr[i].comments ? arr[i].comments : 0,
+            type: arr[i].type
+        });
+
+        if (items[items.length-1].type === 'mediaItem') {
+            items[items.length-1].mediaType = arr[i].mediaType;
+            items[items.length-1].mediaUrl = arr[i].mediaUrl;
+            items[items.length-1].thumbnail = arr[i].thumbnail;
+        }
+
+        if (!original) {
+            items[items.length-1].reference = arr[i].reference;
+            items[items.length-1].referencedUserId = arr[i].referencedUserId;
+        }
+    }
+    return items;
+}
