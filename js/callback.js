80b75bf9e7b23b6288c0e0a7f3e8e2f2299b97e6
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-12-05 18:05:59 +0200

    Some structural changes

--- js/callback.js
+++ js/callback.js
@@ -1,23 +1,5 @@
 "use strict";
 
-/* PUT COLLECTION NAME HERE (As ownerId) */
-var ownerId = 'tasos';
-
-// TODO load email and pass automatically from GTW.
-var user = [];
-user.details = [];
-user.socialTracker = [];
-user.FB = [];
-user.FB.data = [];
-
-user.ST = [];
-user.ST.sources = 0;
-
-user.email = 'tasos@test.com';
-user.password = '123456';
-user.hasFB = false;
-user.hasST = false;
-
 // Callback handling
 // setup the MyRequestCompleted obj
 var MyRequestsCompleted = (function MyRequestsCompleted() {
